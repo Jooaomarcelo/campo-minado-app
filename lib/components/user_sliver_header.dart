c5f7daa267beb8331e015c7dd5d48826f5367afa
Jooaomarcelo <joaomdsb@hotmail.com>
2025-02-14 13:47:11 -0300

    moved sliver header class into a component

---
+++ lib/components/user_sliver_header.dart
@@ -0,0 +1,90 @@
+import 'package:campo_minado_app/core/models/user_data.dart';
+import 'package:flutter/material.dart';
+
+class UserSliverHeader extends SliverPersistentHeaderDelegate {
+  final UserData user;
+
+  double maxHeaderHeight = 200;
+  double minHeaderHeight = kToolbarHeight + 55;
+
+  double maxImageSize = 136;
+  double minImageSize = 40;
+
+  UserSliverHeader(this.user);
+
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    final mediaSize = MediaQuery.of(context).size;
+
+    final percent = shrinkOffset / (maxHeaderHeight - 65);
+    final percent2 = shrinkOffset / maxHeaderHeight;
+
+    final currentImageSize = (maxImageSize * (1 - percent)).clamp(
+      minImageSize,
+      maxImageSize,
+    );
+    final currentImagePosition = ((mediaSize.width / 2 - 65) * (1 - percent))
+        .clamp(minImageSize, maxImageSize);
+
+    return Container(
+      color: Color.fromRGBO(18, 18, 18, 1),
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border(
+            bottom: BorderSide(
+              color: Colors.white
+                  .withValues(alpha: percent2 * 2 < 1 ? percent2 * 2 : 1),
+              width: 0.5,
+            ),
+          ),
+        ),
+        child: Stack(
+          children: [
+            Positioned(
+              top: MediaQuery.of(context).viewPadding.top + 15,
+              left: currentImagePosition + 50,
+              child: Text(
+                user.name,
+                style: TextStyle(
+                  color: Colors.white.withValues(alpha: percent2),
+                  fontSize: 20,
+                ),
+              ),
+            ),
+            Positioned(
+              top: MediaQuery.of(context).viewPadding.top + 5,
+              left: 0,
+              child: BackButton(),
+            ),
+            Positioned(
+              top: MediaQuery.of(context).viewPadding.top + 5,
+              left: currentImagePosition,
+              bottom: 0,
+              child: Container(
+                width: currentImageSize,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    image: NetworkImage(user.imageUrl),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  double get maxExtent => maxHeaderHeight;
+
+  @override
+  double get minExtent => minHeaderHeight;
+
+  @override
+  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) {
+    return false;
+  }
+}
